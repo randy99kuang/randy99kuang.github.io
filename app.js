78b0f494b585491ec6fae4c7dff99e9feae27626
rkuang <rkuang@yext.com>
2020-06-22 23:36:14 -0400

    committing deleted files

--- app.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-console.log('Here\'s a hidden message');
-
-// Inserting Date on the webpage
-let today = new Date();
-// let formatDate = today.toDateString();
-let formatDate = today; 
-let selectElement = document.getElementById('date');
-selectElement.innerHTML = formatDate;
