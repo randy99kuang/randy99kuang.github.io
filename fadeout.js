5d2221ff4de3cc658a9e56fc485005b25af609c2
Ranger Kuang <rkuang8@gmail.com>
2020-07-27 02:22:31 -0400

    added fade in effects and white bars on index.html

--- fadeout.js
+++ fadeout.js
@@ -1,3 +1,28 @@
+
+
 $(window).scroll(function(){
-    $(".transparent-card").css("opacity", 1 - $(window).scrollTop() / 350);
+  $(".fade-out-bars-top").css("opacity", 1.2 - $(window).scrollTop() / 200);
+});
+
+$(window).scroll(function(){
+    $(".transparent-card").css("opacity", 1.5 - $(window).scrollTop() / 200);
   });
+
+$(window).scroll(function(){
+  $(".fade-out-bars-bottom").css("opacity", 1.8 - $(window).scrollTop() / 200);
+});
+
+
+$(function() {
+  $('body').removeClass('fade-out');
+});
+
+
+// var element = document.getElementById("myDIV");
+// element.classList.add("fade-out");
+
+
+// $(function() {
+//   var element = document.getElementById("myDIV");
+//   element.classList.remove('fade-out');
+// });
