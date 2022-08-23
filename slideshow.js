5f7e5882bdf6bf4329aa2be64691ec0bb1697459
Ranger Kuang <rkuang@Rangers-MBP.T-mobile.com>
2022-08-22 22:10:35 -0400

    slideshow try to make automatic

--- slideshow.js
+++ slideshow.js
@@ -20,10 +20,12 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
+  slideIndex++;
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 6000); // Change image every 6 seconds
 }
 
